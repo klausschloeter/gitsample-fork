908b7a9e4524664cb1a60f841384a79861c99112
xgadwbr <xgadwbr@CP12108.inhouse.ad.in.gad.de>
2022-05-13 11:48:28 +0200

    sinnvollen Kommentar hinzugefügt

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -29,7 +29,7 @@ public class Calculator {
 	private float value;
 
 	public static String NAME = "Simple Calculator";
-
+// sinnvoller Kommentar
 	public Calculator(TextProvider textProvider) {
 		this.textProvider = textProvider;
 		setupDefaultOperations();
